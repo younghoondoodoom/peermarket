4d7f84bcb570fed019eb1df6f1450c4bb23a9bf0
ChoiYoungHoon <dudgns2865@naver.com>
2022-05-20 15:56:31 +0900

    feat: exception 추가 및 SecurityConfig 수정

--- src/main/java/peermarket/peershop/config/SecurityConfig.java
+++ src/main/java/peermarket/peershop/config/SecurityConfig.java
@@ -3,18 +3,15 @@ package peermarket.peershop.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import peermarket.peershop.service.MemberService;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리ㅣ 필터가 스프링 필터체인에 등록됨
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
