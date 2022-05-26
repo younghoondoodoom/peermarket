2b3847857551094fc62ce5091c007c54ffdbd385
ChoiYoungHoon <dudgns2865@naver.com>
2022-05-26 12:47:17 +0900

    feat: itemreview entity, repo 작성

---
+++ src/main/java/peermarket/peershop/repository/ItemReviewRepository.java
@@ -0,0 +1,8 @@
+package peermarket.peershop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import peermarket.peershop.entity.ItemReview;
+
+public interface ItemReviewRepository extends JpaRepository<ItemReview, Long> {
+
+}
