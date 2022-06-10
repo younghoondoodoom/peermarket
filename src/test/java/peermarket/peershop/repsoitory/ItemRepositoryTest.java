1d47ba386c3f09b5d577fad6cb93fcb86e3338c4
ChoiYoungHoon <dudgns2865@naver.com>
2022-06-10 16:31:27 +0900

    test: ItemRepositoryTest 아이템 검색 테스트 작성

--- src/test/java/peermarket/peershop/repsoitory/ItemRepositoryTest.java
+++ src/test/java/peermarket/peershop/repsoitory/ItemRepositoryTest.java
@@ -1,10 +1,8 @@
 package peermarket.peershop.repsoitory;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Optional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,7 +13,6 @@ import org.springframework.data.domain.Sort;
 import peermarket.peershop.entity.Item;
 import peermarket.peershop.entity.Member;
 import peermarket.peershop.repository.ItemRepository;
-import peermarket.peershop.repository.ItemReviewRepository;
 import peermarket.peershop.repository.MemberRepository;
 
 @DataJpaTest
@@ -52,6 +49,38 @@ class ItemRepositoryTest {
         assertThat(findOwnItems.getTotalPages()).isEqualTo(3);
         assertThat(findOwnItems.getSize()).isEqualTo(2);
         assertThat(findOwnItems.getContent().get(0).getId()).isEqualTo(item5.getId());
+
+    }
+
+    @Test
+    public void 아이템검색() throws Exception {
+        //given
+        Member member = new Member("test@test.com", "test123!", "test");
+        memberRepository.save(member);
+
+        Item item1 = new Item(member, "item1", "imgpath", "item1", 100, 10000L);
+        Item item2 = new Item(member, "item2", "imgpath", "item2", 100, 10000L);
+        Item item3 = new Item(member, "item3", "imgpath", "item3", 100, 10000L);
+        Item item4 = new Item(member, "item4", "imgpath", "item4", 100, 10000L);
+        Item item5 = new Item(member, "item5", "imgpath", "item5", 100, 10000L);
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+        itemRepository.save(item4);
+        itemRepository.save(item5);
+
+        //when
+        Pageable pageable = PageRequest.of(0, 2, Sort.by("createdAt").descending());
+        Page<Item> findItems = itemRepository.findByItemNameContaining("item", pageable);
+        Page<Item> findNoThing = itemRepository.findByItemNameContaining("noThing", pageable);
+
+        //then
+        assertThat(findItems.getTotalElements()).isEqualTo(5);
+        assertThat(findItems.getSize()).isEqualTo(2);
+        assertThat(findItems.getContent()).contains(item5);
+
+        assertThat(findNoThing.getTotalElements()).isEqualTo(0);
+        assertThat(findNoThing.getContent()).doesNotContain(item5);
 
     }
 
